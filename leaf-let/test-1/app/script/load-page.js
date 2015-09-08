290b8c0c96460a040ba817fae5686c4156389578
Mayur Sarode <sarodems@gmail.com>
2015-09-08 19:27:53 +0530

    onwe
    onwe

--- leaf-let/test-1/app/script/load-page.js
+++ leaf-let/test-1/app/script/load-page.js
@@ -2,11 +2,11 @@ var lblcurrentmsg = document.getElementById('lblcurrentmsg'),
     txtnewmsg = document.getElementById('txtnewmsg'),
     btnupdatemsg = document.getElementById('btnupdatemsg'),
     rootRef = new Firebase('https://alert-msg.firebaseio.com'),
-    currentmsgref = rootRef.child('currentmsgref');
-btnupdatemsg.addEventListener('click', function () {
-    currentmsgref.set(txtnewmsg.value);
+    currentMessageRef = rootRef.child('currentmsgref');
+btnupdatemsg.addEventListener('click', function() {
+    currentMessageRef.set(txtnewmsg.value);
     txtnewmsg.value = '';
 });    
-currentmsgref.on('value', function (snapshot) {
+currentMessageRef.on('value', function(snapshot) {
     lblcurrentmsg.innerText = snapshot.val();
 });
