675545c59d52297a7f50093611fb8371b97d71a9
Mayur Sarode <sarodems@gmail.com>
2015-09-08 17:46:50 +0530

    ems-change
    database and ems msg module is added..

--- leaf-let/test-1/app/script/load-page.js
+++ leaf-let/test-1/app/script/load-page.js
@@ -1,2 +1,18 @@
-var app = document.querySelector('#app');
+var app = document.getElementById('app');
 app.page = 'notice';
+
+
+var lblcurrentmsg = document.getElementById('lblcurrentmsg'),
+    txtnewmsg = document.getElementById('txtnewmsg'),
+    btnupdatemsg = document.getElementById('btnupdatemsg'),
+    rootref = new Firebase('https://alert-msg.firebaseio.com/'),
+    currentmsgref = rootref.child('currentmsgref');
+
+    btnupdatemsg.addEventListener('click', function(){
+        currentmsgref.set(txtnewmsg.value);
+        txtnewmsg.value = '';
+    });
+    
+    currentmsgref.on('value', function(snapshot){
+        lblcurrentmsg.innerText = snapshot.val();
+    });
