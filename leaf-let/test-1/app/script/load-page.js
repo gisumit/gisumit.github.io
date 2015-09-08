436ea69bda6c8d68a020efcd044d28d49674adc9
Mayur Sarode <sarodems@gmail.com>
2015-09-08 18:37:28 +0530

    same error sucks
    same error sucks

--- leaf-let/test-1/app/script/load-page.js
+++ leaf-let/test-1/app/script/load-page.js
@@ -5,13 +5,13 @@ app.page = 'notice';
 var lblcurrentmsg = document.getElementById('lblcurrentmsg'),
     txtnewmsg = document.getElementById('txtnewmsg'),
     btnupdatemsg = document.getElementById('btnupdatemsg'),
-    rootref = new Firebase('https://alert-msg.firebaseio.com/'),
-    currentmsgref = rootref.child('currentmsgref');
+    rootRef = new Firebase('https://alert-msg.firebaseio.com'),
+    currentmsgref = rootRef.child('currentmsgref');
 
-    btnupdatemsg.onclick = function(){
+    btnupdatemsg.addEventListener('click', function(){
         currentmsgref.set(txtnewmsg.value);
         txtnewmsg.value = '';
-    };
+    });
     
     currentmsgref.on('value', function(snapshot){
         lblcurrentmsg.innerText = snapshot.val();
