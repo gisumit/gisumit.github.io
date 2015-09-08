7e1b4967e71a11526cebfd1ae61611ba5151019f
Mayur Sarode <sarodems@gmail.com>
2015-09-08 19:05:29 +0530

    same error
    same error

--- leaf-let/test-1/app/script/load-page.js
+++ leaf-let/test-1/app/script/load-page.js
@@ -3,19 +3,10 @@ var lblcurrentmsg = document.getElementById('lblcurrentmsg'),
     btnupdatemsg = document.getElementById('btnupdatemsg'),
     rootRef = new Firebase('https://alert-msg.firebaseio.com'),
     currentmsgref = rootRef.child('currentmsgref');
-<<<<<<< HEAD
 btnupdatemsg.addEventListener('click', function () {
     currentmsgref.set(txtnewmsg.value);
     txtnewmsg.value = '';
-});
-=======
-
-    btnupdatemsg.addEventListener('click', function(e){
-        currentmsgref.set(txtnewmsg.value);
-        txtnewmsg.value = '';
-    });
->>>>>>> origin/master
-    
+});    
 currentmsgref.on('value', function (snapshot) {
     lblcurrentmsg.innerText = snapshot.val();
 });
