e8fb12c9cd390a8ad543c4267a5637ccdd592751
Sumit <gsumitingole.com@gmail.com>
2015-09-10 11:36:08 +0530

    Update load-page.js

--- leaf-let/test-1/app/script/load-page.js
+++ leaf-let/test-1/app/script/load-page.js
@@ -4,4 +4,5 @@ var lblcurrentmsg = document.getElementById('lblcurrentmsg'),
     rootRef = new Firebase('https://alert-msg.firebaseIO.com'),
     currentMessageRef = rootRef.child('currentmsgref');
 /* btnupdatemsg.addEventListener('click',function(){ currentMessageRef.set(txtnewmsg.value); }); */
+function myFunction(){ currentMessageRef.set(txtnewmsg.value); txtnewmsg.value='';}
 currentMessageRef.on('value', function(snapshot) { lblcurrentmsg.innerText = snapshot.val(); });
